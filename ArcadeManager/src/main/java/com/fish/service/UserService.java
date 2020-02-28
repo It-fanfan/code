fd71dbc51b785c9c488d6ae810885ddfee64c883
It-fanfan <782816806@qq.com>
2020-02-28 18:08:39 +0800

    打算

--- ArcadeManager/src/main/java/com/fish/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.fish.service;
-
-import com.fish.dao.primary.model.User;
-
-public interface UserService
-{
-    User selectByUserName(String userName);
-}
--- manager0216/src/main/java/com/fish/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.fish.service;
-
-import com.fish.dao.primary.model.ManageAccount;
-
-public interface UserService
-{
-    ManageAccount selectByUserName(String userName);
-}
